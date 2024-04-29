9d524cd35683a64287151ae08741247206277f12
Eduardo <edo.carvajal@hotmail.com>
2024-04-29 01:36:20 -0400

    nuevo build

--- src/test/java/com/example/serviciousuario/repository/UsuarioRepositoryTest.java
+++ src/test/java/com/example/serviciousuario/repository/UsuarioRepositoryTest.java
@@ -2,7 +2,6 @@ package com.example.serviciousuario.repository;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
-import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import java.util.Optional;
 
